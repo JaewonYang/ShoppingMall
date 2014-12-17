4d9e7ef1888733a6b5d4409b1260c0bcdbb2dd78
Jaewon <skyboyddd@gmail.com>
2014-12-17 21:30:48 +0900

    modify InputClass
    give showing method getdays() to Ouput Class
    
    modify InputClass   make inputInfo method.

--- Calender/src/before_refactoring/Input.java
+++ Calender/src/before_refactoring/Input.java
@@ -3,35 +3,34 @@ package before_refactoring;
 import java.util.Scanner;
 
 public class Input {
-	private int n1;
-	private int n2;
-
-	public void getdays(int[] days, String[] daysS) {
-		System.out.println("두개의 기념일의 날짜의 차이를 구하는 프로그램입니다. 기념일 두개지를 골라주세요");
-
-		for (int i = 0; i < 18; i++) {
-			System.out.print(i + 1 + ". " + days[i] + " " + daysS[i] + "   ");
-		}
-    System.out.print("\n");
-}
+	private int choosenNumber, choosenFirstNumber, choosenSecondNumber;
+    private int countNumber=0;      // 사용자가 몇번째 입력을 하는지 나타내는 수
+    private Scanner scan ;
 
 	public void inputdata() {
 		while (true) {
-			System.out.println("첫번째 기념일을 고르겠습니다. 기념일 중 하나를 골라주세요.");
-			Scanner scan1 = new Scanner(System.in);
-			n1 = scan1.nextInt();
+            countNumber++;
+            inputInfo(countNumber);
+            choosenFirstNumber =choosenNumber;
+            if (choosenNumber >= 1 & choosenNumber <= 17) {
+                countNumber++;
+                inputInfo(countNumber);
+                choosenSecondNumber =choosenNumber;
+                break;
+            } else {
+                System.out.println("잘못누르셨습니다. ");
+                continue;
+            }
+        }
+    }
 
-			if (n1 >= 1 & n1 <= 17) {
-				System.out.println("두번째 기념일을 고르겠습니다. 기념일중 하나를 골라주세요");
-				Scanner scan2 = new Scanner(System.in);
-				n2 = scan2.nextInt();
-				break;
-			} else {
-				System.out.println("잘못누르셨습니다. ");
-				continue;
-			}
-		}
-	}
-		public int getnum1() {return n1;}
-		public int getnum2() {return n2;}
+    public int getFirstNumber() {return choosenFirstNumber;}
+    public int getSecondNumber() {return choosenSecondNumber;}
+
+    private int inputInfo(int orderNumber){
+            System.out.println(orderNumber+ " 번째 기념일을 고르겠습니다. 기념일 중 하나를 골라주세요.");
+            scan = new Scanner(System.in);
+            choosenNumber = scan.nextInt();
+            return choosenNumber;
+    }
 }
